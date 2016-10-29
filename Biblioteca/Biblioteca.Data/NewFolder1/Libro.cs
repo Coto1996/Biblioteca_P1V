50bc98cff5e5916ed9cd1bc1ff74f036898d6957
Laboratorio <Laboratorio>
2016-10-29 15:08:11 -0600

    Modificaciones3

---
+++ Biblioteca/Biblioteca.Data/NewFolder1/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Data.NewFolder1
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int AñoDePublicacion { get; set; }
+        public Editorial Editorial { get; set; }
+    }
+}
