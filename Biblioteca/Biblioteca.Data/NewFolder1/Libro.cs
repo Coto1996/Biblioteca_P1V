e029559494ecc8cd153871f20c32a3844b7f5d3b
Laboratorio <Laboratorio>
2016-11-19 11:11:52 -0600

    F

--- Biblioteca/Biblioteca.Data/NewFolder1/Libro.cs
+++ Biblioteca/Biblioteca.Data/NewFolder1/Libro.cs
@@ -8,9 +8,21 @@ namespace Biblioteca.Data.NewFolder1
 {
     public class Libro
     {
+
+        public Libro()
+        {
+            this.Autores = new List<Autor>();
+        }
         public int Id { get; set; }
         public string Nombre { get; set; }
         public int AñoDePublicacion { get; set; }
         public Editorial Editorial { get; set; }
+
+        public IList<Autor> Autores { get; set; }
+
+        public void AgregarAutor (Autor nuevoAutor)
+        {
+            this.Autores.Add(nuevoAutor);
+        }
     }
 }
